83b859735afea0a4cf14bada6b835227353d32b1
fuyou <fengyuyao.168@outlook.com>
2026-05-12 21:40:54 +0800

    vscode测试

---
+++ Hello.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <ctime>
+//#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout << "Hello";
+    return 0;
+}
